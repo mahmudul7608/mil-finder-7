5bb3e502ea7902429ebc7cfa4a1dbdf5ffa7bc98
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-10-14 21:22:15 +0600

    add fetch API and show meals

--- script.js
+++ script.js
@@ -23,7 +23,26 @@ function searchMeal(e) {
       .then((data) => {
         console.log(data);
         resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search result. Try again!</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals.map(
+              meal => `
+            <div class="meal">
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+          <div class="meal-info" data-mealID="${meal.idMeal}">
+          <h3>${meal.strMeal}</h3>
+          </div>
+        </div>
+
+            `)
+            .join('');
+        }
       });
+
+    // clear search text
+    search.value = "";
   } else {
     alert("Please enter a search term");
   }
